cf93cd60887f017ab6601fe2acbfb21dd7097f24
Kevin Fortier <kevin.r.fortier@gmail.com>
2023-04-11 20:38:21 -0400

    add another flow

--- flows/__init__.py
+++ flows/__init__.py
@@ -1,5 +1,6 @@
-from flows.hello_flow import hello_world
+from flows.hello_flow import hello_world, hello_world_again
 
 flows = [
-    {"flow": hello_world, "file_name": "hello_world.py"}
+    {"flow": hello_world, "file_name": "hello_world.py"},
+    {"flow": hello_world_again, "file_name": "hello_world.py"}
 ]
--- flows/hello_flow.py
+++ flows/hello_flow.py
@@ -7,3 +7,13 @@ def print_hello(name):
 @flow(name="Hello Flow")
 def hello_world(name="world"):
     print_hello(name)
+
+
+@task
+def print_hello_again(name):
+    print(f"Hello {name}!")
+
+@flow(name="Hello Again Flow")
+def hello_world_again(name="world"):
+    print_hello(name)
+    print_hello_again(name)
